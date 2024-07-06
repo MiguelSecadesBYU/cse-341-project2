8511ecb82952eaf07f0461968a56dcc16daf9c06
MiguelSecadesBYU <sec21002@byui.edu>
2024-07-06 19:43:42 +0200

    updating passportConfig to render.com

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "/auth/github/callback"
+    callbackURL: process.env.CALLBACK_URL
   },
   async function(accessToken, refreshToken, profile, done) {
     try {
